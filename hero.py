4c4626151678ec35867147d4b74b3af2605140a3
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2024-11-18 15:08:42 -0800

    finished with abilites and armor, need to finish fight loop

--- hero.py
+++ hero.py
@@ -9,7 +9,7 @@ class Hero:
         self.starting_health = starting_health
         self.current_health = starting_health
         self.abilities = list()
-        self.armor = list()
+        self.armors = list()
         
         
     
@@ -29,6 +29,7 @@ class Hero:
         # percentage1 = self.starting_health/chance
         # percentage2 = opponent.starting_health/chance
 
+    #Add and Attack with Ability 
     def add_ability(self, ability):
         self.abilities.append(ability)
 
@@ -38,15 +39,38 @@ class Hero:
             total_damage +=ability.attack()
         return total_damage
         
+    #Add and Defend with Armor
+    def add_armor(self,armor):
+        self.armors.append(armor)    
         
-        
-        
+    def defend(self):
+        total_block = 0
+        for armor in self.armors:
+            if self.current_health == 0:
+                print(f"The hero is currently dead and cannot defend")
+            elif self.armors == 0:
+                print(f"The hero has no armor there cannot defend")
+            else:
+                total_block += armor.block()
+        return total_block
+    
+    #Take Damage Method
+    def take_damage(self, damage):
+        defense = self.defend()
+        temp = damage - defense
+        self.current_health -=temp      
+      
+    #Is Alive Method 
+    def is_alive(self):
+        if self.current_health <=0 :
+            return False
+        elif self.current_health >0:
+            return True  
         
         
 if __name__ =="__main__":
-    ability = Ability("Great Debugging", 50)
-    another_ability = Ability("Smarty Pants", 90)
     hero = Hero("Grace Hopper", 200)
-    hero.add_ability(ability)
-    hero.add_ability(another_ability)
-    print(hero.attack())
+    hero.take_damage(150)
+    print(hero.is_alive())
+    hero.take_damage(15000)
+    print(hero.is_alive())
