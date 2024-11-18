6e61bd0c8140c52119185caf1657cb4ca80d02da
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2024-11-18 14:52:24 -0800

    Started adding to hero class

--- hero.py
+++ hero.py
@@ -1,10 +1,17 @@
 import random
+from ability import Ability
+from armor import Armor
+
 
 class Hero:
     def __init__(self, name, starting_health = 100):
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
+        self.abilities = list()
+        self.armor = list()
+        
+        
     
     #Fight method 
     def fight (self,opponent):
@@ -16,6 +23,20 @@ class Hero:
         winner = random.choice([self,opponent])
         print(f"{winner.name} is the winner of this match." )
         
+        #Strech Goal for Fight Method
+        
+        # chance = self.starting_health + opponent.starting_health
+        # percentage1 = self.starting_health/chance
+        # percentage2 = opponent.starting_health/chance
+
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage +=ability.attack()
+        return total_damage
         
         
         
@@ -23,10 +44,9 @@ class Hero:
         
         
 if __name__ =="__main__":
-    my_hero = Hero ("Grace Hopper", 200)
-    print(my_hero.name)
-    print(my_hero.current_health)
-    hero1 = Hero("Wonder Woman")
-    hero2 = Hero("Dumbledore")
-
-    hero1.fight(hero2)
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
