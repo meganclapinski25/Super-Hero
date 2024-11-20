af55255ae0ff61a7014ebdeffd76c03069a1022f
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2024-11-20 10:42:44 -0800

    Pytest working, changed list and print statment

--- team.py
+++ team.py
@@ -1,22 +1,26 @@
 class Team:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
-        self.heroes = list()
-        def remove_hero(self, name):
-            foundHero = False
-            for hero in self.heroes:
-                if hero.name == name:
-                    self.heroes.remove(hero)
-                foundHero = True
-            if not foundHero:
-                return 0
-        def view_all_heroes(self):
-            for hero in self.heroes:
-                print(f"{hero.name}")
-            
-        
-        def add_hero(self, hero):
+        self.heroes = []
+    def add_hero(self, hero):
             self.heroes.append(hero)
-            
-if __name__ =="__main__":
+
+    def remove_hero(self, name):
+        foundHero = False
+        for hero in self.heroes:
+            if hero.name == name:
+                self.heroes.remove(hero)
+                foundHero = True
+        if not foundHero:
+            return 0
+        
+
+    def view_all_heroes(self):
+        for hero in self.heroes:
+            print(hero.name)
+
+    
+
+
+if __name__ == "__main__":
     print('hello')
